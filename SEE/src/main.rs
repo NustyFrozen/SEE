e2ca7a99c030acd7687720b9ee8e4e7e54f11b21
NustyFrozen <43705995+NustyFrozen@users.noreply.github.com>
2026-03-19 03:15:28 +0200

    and back to rust
    C# -> Rust
    I am not giving up on learning rust yet!

--- SEE/src/main.rs
+++ SEE/src/main.rs
@@ -1,4 +1,23 @@
-mod mdfetch;
-fn main() {
-    println!("Hello, world!");
+mod tui;
+use journald::reader::{JournalReader, JournalReaderConfig};
+use tui::SEETui;
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Open the journal with a default configuration
+    let mut gui = tui::SEETui::new();
+    gui.run();
+    let mut reader = JournalReader::open(&JournalReaderConfig::default())?;
+    reader.add_filter("PRIORITY=3");
+    // Iterate over available entries
+    while let Some(entry) = reader.next_entry()? {
+        println!(
+            ": p={} m={}",
+            entry.get_field("PRIORITY").unwrap_or("unknown"),
+            entry.get_field("MESSAGE").unwrap_or("unknown")
+        );
+        // You can also access other fields like "PRIORITY", "_SYSTEMD_UNIT", etc.
+        // entry.get("PRIORITY"), entry.get("_SYSTEMD_UNIT")
+    }
+
+    Ok(())
 }
